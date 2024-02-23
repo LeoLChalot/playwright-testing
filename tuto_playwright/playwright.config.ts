dafd93150ab429d7d554c4878a3b114e3c3854c5
LeoLChalot <77191750+LeoLChalot@users.noreply.github.com>
2024-02-23 16:11:04 +0100

    Reorganization tests folder

--- tuto_playwright/playwright.config.ts
+++ tuto_playwright/playwright.config.ts
@@ -3,28 +3,24 @@ import { PlaywrightTestConfig, devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   // Look for test files in the "tests" directory, relative to this configuration file.
   testDir: 'tests',
-  timeout: 5_000,
+  timeout: 8_000,
+  retries: 1,
   use: {
-    // Base URL to use in actions like `await page.goto('/')`.
-    baseURL: 'http://127.0.0.1:3000',
-
-    // Collect trace when retrying the failed test.
-    trace: 'on-first-retry',
+    trace: 'on-first-retry'
   },
+  // use: {
+  //   // Base URL to use in actions like `await page.goto('/')`.
+  //   baseURL: 'http://127.0.0.1:3000',
+
+  //   // Collect trace when retrying the failed test.
+  //   trace: 'on-first-retry',
+  // },
   // Configure projects for major browsers.
   projects: [
     {
-      name: 'brave',
-      use: { ...devices['Desktop Brave'] },
-    },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
       name: 'chrome',
       use: { ...devices['Desktop Chrome'] },
-    }
+    },
   ],
   // Run your local dev server before starting the tests.
   // webServer: {
--- tuto_playwright/tests/playwright.test.ts
+++ tuto_playwright/tests/playwright.test.ts
@@ -1,47 +1,41 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
-    // Expect a title "to contain" a substring.
-    // await page.pause();
-    await expect(page).toHaveTitle(/Playwright/);
-});
+test.describe('Playwright', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://playwright.dev/');
+        await expect(page).toHaveTitle(/Playwright/);
+    })
 
-test('(Long Syntaxe) Get Started link', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
-    // Click the get started link.
-    const getStarted = page.getByRole('link', { name: 'Get Started' });
-    await getStarted.click();
-    // Expects the URL to contain intro.
-    await expect(page).toHaveURL(/.*intro/);
-});
+    test('(Long Syntaxe) Get Started link', async ({ page }) => {
+        // Click the get started link.
+        const getStarted = page.getByRole('link', { name: 'Get Started' });
+        await getStarted.click();
+        // Expects the URL to contain intro.
+        await expect(page).toHaveURL(/.*intro/);
+    });
 
-// Same test as above but with shorter syntax
-test('(Short Syntaxe) Get Started link', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
-    // Click the get started link.
-    await page.click('text=Get Started');
-    // Expects the URL to contain intro.
-    await expect(page).toHaveURL(/.*intro/);
-});
+    // Same test as above but with shorter syntax
+    test('(Short Syntaxe) Get Started link', async ({ page }) => {
+        // Click the get started link.
+        await page.click('text=Get Started');
+        // Expects the URL to contain intro.
+        await expect(page).toHaveURL(/.*intro/);
+    });
 
-// Same test as below but with locator syntax
-test('(Locator) Get Started link', async ({ page }) => {
-    // Go to the starting url
-    await page.goto('https://playwright.dev/');
+    // Same test as below but with locator syntax
+    test('(Locator) Get Started link', async ({ page }) => {
+        // Get the get started link element
+        const getStarted = page.locator('text=Get Started');
+        // Check if the get started element has an href
+        await expect(getStarted).toHaveAttribute('href', '/docs/intro');
+        // Click the get started link
+        await getStarted.click();
+        // Expects the aims URL to contain intro.
+        await expect(page).toHaveURL(/.*intro/);
+        // Expect the text "Introduction" to be visible
+        await expect(page.locator('text=Introduction').first()).toBeVisible();
+    });
+})
 
-    // Get the get started link element
-    const getStarted = page.locator('text=Get Started');
 
-    // Check if the get started element has an href
-    await expect(getStarted).toHaveAttribute('href', '/docs/intro');
 
-    // Click the get started link
-    await getStarted.click();
-
-    // Expects the aims URL to contain intro.
-    await expect(page).toHaveURL(/.*intro/);
-
-    // Expect the text "Introduction" to be visible
-    await expect(page.locator('text=Introduction').first()).toBeVisible();
-});
